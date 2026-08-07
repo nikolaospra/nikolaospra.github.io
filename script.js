db924483e85531eb5092e7f1fc9e4f24391ec569
Fr. Nikolaos Prasinos <nikolaos.pra@gmail.com>
2026-08-08 01:16:42 +0300

    u

---
+++ script.js
@@ -0,0 +1,1 @@
+fetch('events.json').then(r=>r.json()).then(d=>{let u=document.getElementById('events');d.forEach(e=>{let l=document.createElement('li');l.textContent=e.date+' '+e.time+' '+e.title;u.appendChild(l);});});
